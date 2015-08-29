3cb084af827fc5ebd87856ac13307084b674e1c0
Viacheslav Mayorov <mayorovvn@gmail.com>
2015-08-29 19:02:10 +0300

    use the sale price for the products ordering

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -5,6 +5,14 @@ module ProductExtensions
     delegate_belongs_to :master, 
                           :sale_price, :original_price, :on_sale?,
                           :display_sale_price, :display_original_price
+
+    add_search_scope :ascend_by_master_price do
+      joins(:master => :default_price).order("coalesce(#{price_table_name}.sale_price, #{price_table_name}.amount) ASC")
+    end
+
+    add_search_scope :descend_by_master_price do
+      joins(:master => :default_price).order("coalesce(#{price_table_name}.sale_price, #{price_table_name}.amount) DESC")
+    end
   end
 end
 
